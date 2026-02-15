29108599805a2c894ee8c2b9c493bab70850e396
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-02-15 17:09:22 +0000

    Time: 73 ms (47.6%), Space: 19.7 MB (43.9%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,34 +1,23 @@
 class Codec:
-    """NeetCode uses Codec class; LeetCode variants may call methods directly."""
-
     def encode(self, strs: List[str]) -> str:
-        parts: List[str] = []
+        """Encodes a list of strings to a single string.
+        """
+        parts = []
         for s in strs:
             parts.append(str(len(s)))
-            parts.append('#')
+            parts.append("#")
             parts.append(s)
         return ''.join(parts)
 
     def decode(self, s: str) -> List[str]:
-        res: List[str] = []
+        """Decodes a single string to a list of strings.
+        """
         i = 0
-        n = len(s)
-
-        while i < n:
-            # find the delimiter '#' that ends the length field
-            j = i
-            while j < n and s[j] != '#':
-                j += 1
-
-            # Safety: if no '#', input is malformed
-            if j == n:
-                raise ValueError("Malformed encoded string: missing length delimiter")
-
-            length = int(s[i:j])  # length can be 0
-            i = j + 1
-
-            # slice out the next `length` characters
-            res.append(s[i:i + length])
-            i += length
-
-        return res
+        out = []
+        while i < len(s):
+            j = s.find('#', i)
+            num = int(s[i:j]) # not include j
+            i = j+1
+            out.append(s[i: i + num])
+            i += num
+        return out
