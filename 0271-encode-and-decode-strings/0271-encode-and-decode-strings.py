793f9e76da8e3acc05754adab045ef183e9705f4
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-02-15 17:11:02 +0000

    Time: 81 ms (23.18%), Space: 19.6 MB (43.9%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -15,7 +15,9 @@ class Codec:
         i = 0
         out = []
         while i < len(s):
-            j = s.find('#', i)
+            j = i
+            while j < len(s) and s[j] != '#':
+                j += 1
             num = int(s[i:j]) # not include j
             i = j+1
             out.append(s[i: i + num])
