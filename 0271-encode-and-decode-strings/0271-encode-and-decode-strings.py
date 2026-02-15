ad070f5758ea638f16d546fde6e4203a0a4d76bf
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-02-15 16:41:00 +0000

    Time: 75 ms (40.1%), Space: 19.8 MB (24.29%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,26 +1,34 @@
 class Codec:
+    """NeetCode uses Codec class; LeetCode variants may call methods directly."""
+
     def encode(self, strs: List[str]) -> str:
-        result = []
-        
-        for string in strs:
-            for c in string:
-                result.append(str(ord(c)))
-                result.append(';')
-            result.append(' ')
-                
-        return ''.join(result)
-        
+        parts: List[str] = []
+        for s in strs:
+            parts.append(str(len(s)))
+            parts.append('#')
+            parts.append(s)
+        return ''.join(parts)
 
     def decode(self, s: str) -> List[str]:
-        result = []
-        strings = s.split(' ')
-		
-        for string in strings:
-            temp = []
-            for c in string.split(';'):
-                if c:
-                    temp.append(chr(int(c)))
-            result.append(''.join(temp))
-			
-        result.pop()
-        return result
+        res: List[str] = []
+        i = 0
+        n = len(s)
+
+        while i < n:
+            # find the delimiter '#' that ends the length field
+            j = i
+            while j < n and s[j] != '#':
+                j += 1
+
+            # Safety: if no '#', input is malformed
+            if j == n:
+                raise ValueError("Malformed encoded string: missing length delimiter")
+
+            length = int(s[i:j])  # length can be 0
+            i = j + 1
+
+            # slice out the next `length` characters
+            res.append(s[i:i + length])
+            i += length
+
+        return res
