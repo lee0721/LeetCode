0704035a246340decf050ff32e54705f5e8b2ec0
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-02-16 17:51:55 +0000

    Time: 3 ms (78.59%), Space: 19.5 MB (26.71%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == '.':
+                    continue
+                b = (r//3) * 3 + (c//3) # number the nine 3x3 zonal squares as 0, 1,..8
+
+                if val in rows[r] or val in cols[c] or val in boxes[b]:
+                    return False
+                
+                rows[r].add(val)
+                cols[c].add(val)
+                boxes[b].add(val)
+        return True
