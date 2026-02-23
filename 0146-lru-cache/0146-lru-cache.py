2d8048cfaae075920f24eca1093828cbb66f92cb
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-02-23 22:18:53 +0000

    Time: 123 ms (59.92%), Space: 77.2 MB (52.57%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,48 @@
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dict = {}
+
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def add(self, node):
+        previous_end = self.tail.prev # tail.prev is MRU
+        previous_end.next = node
+        node.prev = previous_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def get(self, key: int) -> int:
+        if key not in self.dict: return -1
+        node = self.dict[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict: 
+            old_node = self.dict[key]
+            self.remove(old_node)
+            
+        node = ListNode(key, value)
+        self.dict[key] = node
+        self.add(node)
+
+        if len(self.dict) > self.capacity:
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            del self.dict[node_to_delete.key]
