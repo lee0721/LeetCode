5504d7c09652b3c605a89bf720b44306b79d2840
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-02-25 15:42:15 +0000

    Time: 0 ms (100%), Space: 19.4 MB (52.6%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -8,8 +8,13 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root: return None
 
-        right = self.invertTree(root.right)
-        left = self.invertTree(root.left)
-        root.left = right
-        root.right = left
+        # create a queue to store nodes whose left and right child have not been swapped yet
+        queue = deque([root]) # deque() need iterable obj, TreeNode is not iterable
+        # need to put root into list
+        while queue:
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+
+            if current.left: queue.append(current.left)
+            if current.right: queue.append(current.right)
         return root
