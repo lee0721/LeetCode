8b1d267d8adedc609861fe49f426d27edf68952c
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-02-26 13:44:39 +0000

    Time: 0 ms (100%), Space: 19.5 MB (28.6%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,8 +4,18 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    def check(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q: return True
         if not p or not q: return False
         if p.val != q.val: return False
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        return True
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        deq = deque([(p, q)]) 
+        #use tuple to store p and q and use list to pack them bc deque() need the iterable obj
+        while deq:
+            p, q = deq.popleft()
+            if not self.check(p, q): return False
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+        return True
