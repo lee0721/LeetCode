e76e9fc1ecd1ed1061598fbf2a7322eae514ef36
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-03-01 20:56:35 +0000

    Time: 0 ms (100%), Space: 20.2 MB (93.83%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,20 +1,20 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        idx = {}
-        for i, v in enumerate(inorder):
-            idx[v] = i
-        pre_i = 0
-        def build_subtree(inL: int, inR: int) -> Optional[TreeNode]:
-            nonlocal pre_i # allow build_subtree() to advance outer preorder pointer (pre_i += 1)
-            if inL > inR: 
-                return None
-            root_val = preorder[pre_i]
-            pre_i += 1
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        stack = [root]
+        inorder_i = 0
 
-            mid = idx[root_val]
-            root = TreeNode(root_val)
-            root.left = build_subtree(inL, mid-1)
-            root.right = build_subtree(mid+1, inR)
-            return root
-
-        return build_subtree(0, len(inorder) - 1)
+        for v in preorder[1:]: # start from preorder[1]
+            node = stack[-1]
+            if node.val != inorder[inorder_i]:
+                node.left = TreeNode(v)
+                stack.append(node.left)
+            else:
+                while stack and stack[-1].val == inorder[inorder_i]:
+                    node = stack.pop()
+                    inorder_i += 1
+                node.right = TreeNode(v)
+                stack.append(node.right)
+        return root
