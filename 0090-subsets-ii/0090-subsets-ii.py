db88a260cc769936109f91e871d001b593072069
yuhan lee <92746404+lee0721@users.noreply.github.com>
2026-03-09 20:32:00 +0000

    Time: 0 ms (100%), Space: 19.4 MB (85.41%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -6,10 +6,12 @@ class Solution:
         self.subsetsWithDuphelper(nums, subsets, currsubsets, 0)
         return subsets
     def subsetsWithDuphelper(self, nums, subsets, currsubsets, idx):
+        # Add the subset formed so far to the subsets list.
         subsets.append(currsubsets.copy())
         for i in range(idx, len(nums)):
+            # If the current element is a duplicate, ignore.
             if i != idx and nums[i] == nums[i-1]:
-                continue
+                continue # same-level duplicate: would start the same subtree again -> duplicate subsets
             currsubsets.append(nums[i])
             self.subsetsWithDuphelper(nums, subsets, currsubsets, i+1)
             currsubsets.pop()
